4e2c49accc27191fcf2ee28e74f69c3447f135bf
user <user@usernoMacBook-puro.local>
2021-08-16 23:08:28 +0900

    config add

---
+++ contact/config.php
@@ -0,0 +1,17 @@
+<?php 
+//テスト
+define('DB_DNS', 'mysql:host=localhost; dbname=contact_form; charset=utf8');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+
+//本番
+// $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+// $db['dbname'] = ltrim($db['path'], '/');
+// $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+// $user = $db['user'];
+// $password = $db['pass'];
+// define('DB_DNS', $dsn);
+// define('DB_USER', $user);
+// define('DB_PASSWORD', $password);
+
+?>
